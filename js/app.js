e14d0a72fbbd19edfd71548a82ea2e9fa6fb2f6a
ALexander Davis <ajdavis2424@gmail.com>
2023-05-20 13:53:15 -0400

    calorie calculations and display

---
+++ js/app.js
@@ -0,0 +1,110 @@
+class CalorieTracker {
+  constructor() {
+    this._calorieLimit = 2000;
+    this._totalCalories = 0;
+    this._meals = [];
+    this._workouts = [];
+
+    //Constructor runs immediately when you instantiate the class, so display calories etc
+    this._displayCaloriesLimit();
+    this._displayCaloriesTotal();
+    this._displayCaloriesConsumed();
+    this._displayCaloriesBurned();
+    this._displayCaloriesRemaining();
+  }
+  //Public Methods/API
+  addMeal(meal) {
+    //push meal obj
+    this._meals.push(meal);
+
+    //account for calories in meal obj-- total calories = total calories plus meal calories
+    this._totalCalories += meal.calories;
+    //render everytime you add a meal
+    this._render();
+  }
+
+  addworkout(workout) {
+    this._workouts.push(workout);
+    //account for calories in workout -- total calories = total calories minus workout calories
+    this._totalCalories -= workout.calories;
+    //render everytime you add a workout
+    this._render();
+  }
+
+  //Private Methods
+  _displayCaloriesTotal() {
+    const totalCaloriesElement = document.getElementById('calories-total');
+    totalCaloriesElement.innerHTML = this._totalCalories;
+  }
+  _displayCaloriesLimit() {
+    const calorieLimitElement = document.getElementById('calories-limit');
+    calorieLimitElement.innerHTML = this._calorieLimit;
+  }
+
+  _displayCaloriesConsumed() {
+    const caloriesConsumedElement =
+      document.getElementById('calories-consumed');
+    //take all calories in our consumed array to get 1 value
+    const consumed = this._meals.reduce(
+      (total, meal) => total + meal.calories,
+      0
+    );
+    caloriesConsumedElement.innerHTML = consumed;
+  }
+  _displayCaloriesBurned() {
+    const caloriesBurnedElement = document.getElementById('calories-burned');
+    //take all calories in our consumed array to get 1 value
+    const burned = this._workouts.reduce(
+      (total, workout) => total + workout.calories,
+      0
+    );
+    caloriesBurnedElement.innerHTML = burned;
+  }
+
+  _displayCaloriesRemaining() {
+    //total calories from calorie limit
+    const caloriesRemainingElement =
+      document.getElementById('calories-remaining');
+
+    const remaining = this._calorieLimit - this._totalCalories;
+
+    caloriesRemainingElement.innerHTML = remaining;
+  }
+
+  _render() {
+    this._displayCaloriesTotal();
+    this._displayCaloriesConsumed();
+    this._displayCaloriesBurned();
+    this._displayCaloriesRemaining();
+  }
+}
+
+class Meal {
+  constructor(name, calories) {
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
+}
+class Workout {
+  constructor(name, calories) {
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
+}
+
+const tracker = new CalorieTracker();
+
+//create new meal & workout
+const breakfast = new Meal('Breakfast', 400);
+const lunch = new Meal('Lunch', 350);
+tracker.addMeal(breakfast);
+tracker.addMeal(lunch);
+
+const run = new Workout('Morning Run', 300);
+
+tracker.addworkout(run);
+console.log(tracker._meals);
+console.log(tracker._meals);
+console.log(tracker._totalCalories);
